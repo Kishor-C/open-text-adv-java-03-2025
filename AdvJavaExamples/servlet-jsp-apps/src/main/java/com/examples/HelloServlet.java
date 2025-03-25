53ddf46b438ab5cf1d3e9d4e9e9621435204eea6
Kishor <kishor1124@gmail.com>
2025-03-25 13:03:28 +0530

    added entity class

--- AdvJavaExamples/servlet-jsp-apps/src/main/java/com/examples/HelloServlet.java
+++ AdvJavaExamples/servlet-jsp-apps/src/main/java/com/examples/HelloServlet.java
@@ -16,13 +16,14 @@ public class HelloServlet extends HttpServlet {
        
     //GET type request is sent via URL
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter(); //creates an output writer that writes to the response body
+		//PrintWriter out = response.getWriter(); //creates an output writer that writes to the response body
 		String name = "Kishor";
 		// to share the data you can use different objects like request, HttpSession
 		request.setAttribute("username", name); // username is the key & name is the value
 		// RequestDispatcher - it dispatches the request from one resource to another
 		RequestDispatcher rd = request.getRequestDispatcher("display.jsp"); // ${username}
 		rd.forward(request, response); // forwards the request & response will be from display.jsp
+		
 	}
 
 	//POST type request is sent via form method = "post"
